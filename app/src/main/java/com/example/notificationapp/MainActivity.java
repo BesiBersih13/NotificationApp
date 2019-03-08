8ddf2a7f1a6395baeb8a9468fde6c8740aa59564
BesiBersih13 <abdullahazzam98@mail.ugm.ac.id>
2019-03-08 14:26:48 +0700

    MA sendNotif

--- app/src/main/java/com/example/notificationapp/MainActivity.java
+++ app/src/main/java/com/example/notificationapp/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.notificationapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManager notificationManager;
+    private static final int NOTIFICATION_ID=0;
+
+    private Button notifyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notifyButton=(Button)findViewById(R.id.notify);
+
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+    }
+
+    public void sendNotification(){
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("Content Title")
+                .setContentText("Content Notification")
+                .setSmallIcon(R.mipmap.ic_launcher_round);
+
+        Notification notification=notifyBuilder.build();
+        notificationManager.notify(NOTIFICATION_ID,notification);
     }
 }
